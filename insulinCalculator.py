f0f42010e98e196bd0cc8c0b6fa320ce8d43a93f
Ryan Turner <98337469+rcturner7@users.noreply.github.com>
2022-05-02 00:17:48 -0500

    Update insulinCalculator.py

--- insulinCalculator.py
+++ insulinCalculator.py
@@ -8,7 +8,12 @@ units_of_insulin = ratio / scale
 
 total_units = units_of_insulin
 
-if (blood_sugar >= 200) and (blood_sugar < 225):
+if blood_sugar < 70:
+    take_less_carbs = ratio - 35
+    units_of_insulin = take_less_carbs / scale
+    total_units = units_of_insulin
+    print(f"You need to take {total_units} units of insulin. Take adjusted less insulin, eat your food, recheck your glucose levels in 15 to 30 minutes, if glucose levels do not rise above 70 intake 30 more carbs, recheck glucose levels again if needed, and be safe.")
+elif (blood_sugar >= 200) and (blood_sugar < 225):
     total_units = units_of_insulin + 1
     print(f"You need to take {total_units} units of insulin. Enjoy your meal.")
 elif (blood_sugar >= 225) and (blood_sugar < 250):
